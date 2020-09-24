fa710646375e5ef1f91e2e7dbc524478190dd621
катя <45289493+rfnzrfnzrfnz@users.noreply.github.com>
2020-09-24 17:33:16 +0300

    Add files via upload

--- ft_rev_num.py
+++ ft_rev_num.py
@@ -1,7 +1,13 @@
+from ft_len_num import ft_len_num
+
+
 def ft_rev_num(a):
-    c = a % 10
-    b = a // 10
-    n = a // 100
-    k = b % 10
-    ft_rev_num = str(c) + str(k) + str(n)
-    return ft_rev_num
+    k = ft_len_num(a)
+    s = k
+    o = 0
+    c = a
+    for i in range(k):
+        o += (c % 10) * 10 ** s
+        c //= 10
+        s -= 10
+    return o // 10
